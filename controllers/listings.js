336eeedaea910e4fcef264df1f4c49ec86fe1467
Abbas Hakimi <125572292+abbas-hakimi@users.noreply.github.com>
2025-05-16 20:58:51 +0530

    Add category filtering functionality

--- controllers/listings.js
+++ controllers/listings.js
@@ -4,82 +4,81 @@ const mapToken = process.env.MAP_TOKEN;
 const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 module.exports.index = async (req,res)=>{
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs",{allListings});
+    const { category } = req.query;
+    let allListings;
+    
+    if (category) {
+        allListings = await Listing.find({ category });
+    } else {
+        allListings = await Listing.find({});
     }
     
-    module.exports.renderNewForm = (req,res)=>{
-     
-        res.render("listings/new.ejs");
-    }
+    res.render("listings/index.ejs", { allListings });
+}
 
-    module.exports.createListing = async (req,res,next)=>{
+module.exports.renderNewForm = (req,res)=>{
+    res.render("listings/new.ejs");
+}
 
-        let response = await geocodingClient.forwardGeocode({
-          query: req.body.listing.location,
-         limit: 1
-        })
-        .send();
+module.exports.createListing = async (req,res,next)=>{
+    let response = await geocodingClient.forwardGeocode({
+        query: req.body.listing.location,
+        limit: 1
+    }).send();
 
-      
+    let url = req.file.path;
+    let filename = req.file.filename;
+    let newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
+    newListing.image = {url, filename};
+    newListing.geometry = response.body.features[0].geometry;
+    let savedListing = await newListing.save();
+    console.log(savedListing);
+    req.flash("success", "New listing created.");
+    res.redirect("/listings");
+}
 
-          let url = req.file.path;
-          let filename = req.file.filename;
-        let newListing =  new Listing(req.body.listing);
-        newListing.owner = req.user._id; // saving the owner
-        newListing.image= {url,filename}; //saving the image
-        newListing.geometry = response.body.features[0].geometry; // saving coordinates from geocoding mapbox api
-        let savedListing = await newListing.save();
-        console.log(savedListing);
-        req.flash("success","New listing created.");
-        res.redirect("/listings");
-      
-    }
-
-   module.exports.showListing = async (req,res)=>{
-    let {id}= req.params;
-    let listing =  await Listing.findById(id).populate({path: "reviews", populate:{path:"author"}}).populate("owner");
+module.exports.showListing = async (req,res)=>{
+    let {id} = req.params;
+    let listing = await Listing.findById(id)
+        .populate({path: "reviews", populate:{path:"author"}})
+        .populate("owner");
     if(!listing){
-        req.flash("error","Listing you requested for does not exist!");
-       return  res.redirect("/listings");
+        req.flash("error", "Listing you requested for does not exist!");
+        return res.redirect("/listings");
     }
-    res.render("listings/show.ejs",{listing});
-    
+    res.render("listings/show.ejs", {listing});
+}
+
+module.exports.renderEditForm = async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    if(!listing){
+        req.flash("error", "Listing you requested for does not exist!");
+        return res.redirect("/listings");
     }
+    let originalImageUrl = listing.image.url;
+    originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250");
+    res.render("listings/edit.ejs", { listing, originalImageUrl });
+}
 
-    module.exports.renderEditForm = async (req, res) => {
-        let { id } = req.params;
-        const listing = await Listing.findById(id);
-        if(!listing){
-            req.flash("error","Listing you requested for does not exist!");
-           return  res.redirect("/listings");
-        }
-        let originalImageUrl = listing.image.url;
-       originalImageUrl= originalImageUrl.replace("/upload" , "/upload/w_250");
-        res.render("listings/edit.ejs", { listing , originalImageUrl });
-      }
-
-      module.exports.updateListing = async (req, res) => {
-        if(!req.body.listing){
-            throw new ExpressError(400,"Send a valid data for listing.");
-           }
-        let { id } = req.params;
-      
-        let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-        if(typeof req.file !== undefined){
+module.exports.updateListing = async (req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    if(typeof req.file !== "undefined"){
         let url = req.file.path;
-          let filename = req.file.filename;
-          listing.image = {url,filename};
-          await listing.save();
-         }
-        req.flash("success","Listing updated.");
-        res.redirect(`/listings/${id}`);
-      }
+        let filename = req.file.filename;
+        listing.image = {url, filename};
+        await listing.save();
+    }
+    req.flash("success", "Listing updated.");
+    res.redirect(`/listings/${id}`);
+}
 
-      module.exports.destroyListing = async (req, res) => {
-        let { id } = req.params;
-        let deletedListing = await Listing.findByIdAndDelete(id);
-        req.flash("success","Listing deleted.");
-        console.log(deletedListing);
-        res.redirect("/listings");
-      }
+module.exports.destroyListing = async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    req.flash("success", "Listing deleted.");
+    console.log(deletedListing);
+    res.redirect("/listings");
+}
--- models/listing.js
+++ models/listing.js
@@ -5,47 +5,50 @@ const Review = require("./review.js")
 
 const listingSchema = new Schema({
     title:{
-        type :String,
-        required : true
+        type: String,
+        required: true
     },
-    description:String,
-    
+    description: String,
     image:{
-    url: String,
-    filename: String
+        url: String,
+        filename: String
     },
-    price:Number,
-    location:String,
-    country:String,
-    reviews :[
+    price: Number,
+    location: String,
+    country: String,
+    category: {
+        type: String,
+        enum: ["trending", "rooms", "iconic-cities", "mountains", "castles", "pools", "camping", "farms", "arctic", "domes", "boats"],
+        required: true
+    },
+    reviews: [
         {
-        type : Schema.Types.ObjectId,
-        ref: "Review"
+            type: Schema.Types.ObjectId,
+            ref: "Review"
         }
     ],
     owner: {
-        type : Schema.Types.ObjectId,
+        type: Schema.Types.ObjectId,
         ref: "User"
     },
     geometry: {
-    type: {
-    type: String,
-    enum: ['Point'],
-    required: true
-  },
-  coordinates: {
-    type: [Number],
-    required: true
-  }
-}
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    }
 });
 
-listingSchema.post("findOneAndDelete",async (listing)=>{
- if(listing){
-    await Review.deleteMany({_id : {$in: listing.reviews}});
- }
+listingSchema.post("findOneAndDelete", async (listing)=>{
+    if(listing){
+        await Review.deleteMany({_id : {$in: listing.reviews}});
+    }
 });
 
-const Listing = mongoose.model("Listing",listingSchema);
+const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
-
